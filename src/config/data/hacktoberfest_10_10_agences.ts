0e4b17e921ef4c697c1011a3e48a5f235b58879e
Jean-Philippe Baconnais <jean-philippe.baconnais@zenika.com>
2023-10-04 17:46:37 +0200

    add changes on event october 10

--- src/config/data/hacktoberfest_10_10_agences.ts
+++ src/config/data/hacktoberfest_10_10_agences.ts
@@ -3,7 +3,7 @@ import {EventSchema} from '../../types/schema';
 
 export const hacktoberfestOctober10Agences: z.infer<typeof EventSchema> = {
 	eventTitle: 'Hacktoberfest @Zenika : Framasoft a presque 20 ans. Ce que nous avons appris',
-	eventDescription: 'L’agence de Zenika Lyon invite Pierre-Yves Gosset, codirecteur de Framasoft, pour vous proposer un REX sur ces vingts années d\'aventure dans le monde de l\'Open source 💪. L\'évènement sera également disponible sur les chaînes Youtube et Twitch de Zenika.',
+	eventDescription: 'L’agence de Zenika Lyon reçoit Pierre-Yves Gosset, codirecteur de Framasoft, pour vous proposer un REX sur ces vingts années d\'aventure dans le monde de l\'Open source 💪. L\'évènement sera également disponible sur les chaînes Youtube et Twitch de Zenika.',
 	eventDate: new Date('2023-10-10T07:39:11.586Z'),
 	eventTime: '18h30 - 20h30',
 	eventLive: false,
